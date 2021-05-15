904375fc01989fdc9363a8e3aae2f785b2341b1b
Your Name <you@example.com>
2021-05-16 07:40:19 +0900

    mod

---
+++ libwiki.pl
@@ -0,0 +1,195 @@
+#!/usr/bin/perl
+#-------------------------------------------------------
+# libwiki.pl
+#   'libwiki.pl' is a library for wikipedia written in Perl.
+#   Analyze 'wikipedia dump xml file' such as
+#   'enwiki-20210501-pages-articles.xml'
+#   or
+#   'enwiki-20210501-pages-articles.xml.bz2'
+#
+# Usage:
+#   Include 'libwiki.pl' from your .pl
+#   Call 3 functions.
+#      libwiki::set_filename()
+#      libwiki::set_handler()
+#      libwiki::do_proc()
+#   Details in sample1_main.pl
+#
+# Author:
+# License:
+#-------------------------------------------------------
+use strict;
+use warnings;
+use utf8;
+use IO::Uncompress::Bunzip2 qw(bunzip2 $Bunzip2Error) ;
+use Encode 'encode';
+package libwiki;
+
+#-------------------------------------------------------
+# data structure
+#
+#my %page = (
+#    "title" => "page_title_name",
+#    "pageid" => 10,
+#    "ns" => 0,
+#    "revisionid" => 71019675,
+#    "text" => ["line1", "lin2", "line3"],
+#);
+#-------------------------------------------------------
+my $dumpfile = "";
+my $handler = "";
+my $inpage = 0;
+my $intext = 0;
+my $inrevision = 0;
+my $incontributor = 0;
+my %page        = ();
+sub set_filename {
+    #print $_[0], "\n";
+    my $tmp = $_[0];
+    $dumpfile = $tmp;
+}
+sub set_handler {
+    $handler = $_[0];
+}
+sub proc_core {
+    $_ = $_[0];
+
+    # convert a HTML character entity reference to a normal character.
+    $_ =~ s/&lt;/</g;
+    $_ =~ s/&gt;/>/g;
+    $_ =~ s/&quot;/"/g;
+    $_ =~ s/&apos;/'/g;
+    $_ =~ s/&amp;/&/g;
+
+    if ( $inpage == 0 ) {
+        if ( $_ =~ /<page>/ ) {
+            $inpage = 1;
+            return;
+        }
+    }
+    if ( $_ =~ /<\/page>/ ) {
+        if ( exists($page{'title'}) ) {
+            $handler->(\%page);
+        }
+        $inpage = 0;
+        $intext = 0;
+        $inrevision = 0;
+        $incontributor = 0;
+        %page        = ();
+        return;
+    }
+    if ( $_ =~ /<ns>(.*)<\/ns>/ ) {
+        $page{'ns'} = $1;
+    }
+    if ( $_ =~ /<id>(.*)<\/id>/ ) {
+        if ( $inrevision == 0 ) {
+            $page{'pageid'} = $1;
+        } if ( $incontributor == 1 ) {
+            $page{'contributorid'} = $1;
+        } else {
+            $page{'revisionid'} = $1;
+        }
+    }
+    if ( $_ =~ /<parentid>(.*)<\/parentid>/ ) {
+        $page{'parentid'} = $1;
+    }
+    if ( $_ =~ /<username>(.*)<\/username>/ ) {
+        $page{'username'} = $1;
+    }
+    if ( $_ =~ /<ip>(.*)<\/ip>/ ) {
+        $page{'ip'} = $1;
+    }
+    if ( $_ =~ /<timestamp>(.*)<\/timestamp>/ ) {
+        $page{'timestamp'} = $1;
+    }
+    if ( $_ =~ /<model>(.*)<\/model>/ ) {
+        $page{'model'} = $1;
+    }
+    if ( $_ =~ /<format>(.*)<\/format>/ ) {
+        $page{'format'} = $1;
+    }
+    if ( ! exists($page{'title'}) ) {
+        if ( $_ =~ /<title>(.*)<\/title>/ ) {
+            $page{'title'} = $1;
+            return;
+        }
+    }
+    if ( $intext == 0 ) {
+        if ( $_ =~ /<text/ ) {
+            $intext = 1;
+            $_ =~ s/.*xml:space="preserve">//;
+            chomp();
+            push( @{$page{'text'}} , $_);
+            return;
+        }
+    }
+    if ( $_ =~ /<\/text>/ ) {
+        $_ =~ s/<\/text>//;
+        chomp();
+        push( @{$page{'text'}} , $_);
+        $intext = 0;
+        return;
+    }
+    if ( $intext == 1 ) {
+        chomp();
+        push( @{$page{'text'}} , $_);
+        return;
+    }
+    if ( $inrevision == 0 ) {
+        if ( $_ =~ /<revision/ ) {
+            $inrevision = 1;
+            return;
+        }
+    }
+    if ( $_ =~ /<\/revision>/ ) {
+        $inrevision = 0;
+        return;
+    }
+    if ( $incontributor == 0 ) {
+        if ( $_ =~ /<contributor/ ) {
+            $incontributor = 1;
+            return;
+        }
+    }
+    if ( $_ =~ /<\/contributor>/ ) {
+        $incontributor = 0;
+        return;
+    }
+}
+sub is_bz2 {
+    my $dumpfile = $_[0];
+    my $pos = index($dumpfile, ".bz2");
+    #print "pos=$pos\n";
+    if ( $pos != -1 ) {
+        return 1;
+    }
+    return 0;
+}
+sub do_loop_bz2 {
+    my $dumpfile = $_[0];
+    my $gz = new IO::Uncompress::Bunzip2 $dumpfile
+        or die "Cannot open $dumpfile: $IO::Uncompress::Bunzip2::Bunzip2Error\n" ;
+    while (<$gz>) {
+        $_ = Encode::decode('UTF-8',$_);
+        &proc_core($_);
+    }
+    $gz->close();
+}
+sub do_loop_xml {
+    my $dumpfile = $_[0];
+    open( DUMP, "<:encoding(UTF-8)", "$dumpfile" );
+    while (<DUMP>) {
+        &proc_core($_);
+    }
+    close DUMP;
+}
+sub do_proc {
+    if ( &is_bz2($dumpfile) ) {
+        &do_loop_bz2($dumpfile);
+    } else {
+        &do_loop_xml($dumpfile);
+    }
+
+}
+
+1;
---
+++ sample_main1.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use utf8;
+
+# disable output buffering
+$| = 1;
+
+binmode( STDOUT, ":encoding(UTF-8)" );
+
+require 'libwiki.pl';
+
+
+# main function
+sub main {
+    my $dumpfile = "";
+
+    if ( @ARGV < 1 ) {
+        print "Usage: main.pl <filename>\n";
+        exit 1;
+    }
+    $dumpfile = $ARGV[0];
+    print $dumpfile, "\n";
+    libwiki::set_filename($dumpfile);
+    libwiki::set_handler(\&handler);
+    libwiki::do_proc(); # this will take a time over 30 minutes!
+}
+
+# a callback function for each page
+sub handler {
+    #print "handler start!\n";
+    my $page = $_[0];
+
+    #---- title ---------------------------
+    my $title = $$page{'title'}; # need two '$' signs because 'page' variable is a reference of hash.
+    print "title = $title\n";
+    if ( $title eq "" ) {
+        return;
+    }
+    my $ns = $$page{'ns'};
+    print "ns = $ns\n";
+    my $pageid = $$page{'pageid'};
+    print "pageid = $pageid\n";
+    my $revisionid = $$page{'revisionid'};
+    print "revisionid = $revisionid\n";
+
+    #---- text ---------------------------
+    foreach (@{$$page{'text'}}) {
+        my $line = $_;
+        if ( $_ =~ /invalid code/ ) {
+            print "* [[:$title]]\n";
+            print "*: <nowiki>[$_]</nowiki>\n";
+        }
+    }
+}
+
+
+&main();
